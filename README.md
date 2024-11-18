1b14dd0f0dc95b11c0325514ba12f02fe4ff5363
warroyo <warroyo7199008@gmail.com>
2024-11-18 16:05:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# TPK8s DNS
+
+This repo provides a way to add support for many different DNS providers to Tanzu Platform. This is not a Tanzu officially supported method, it falls under the "bring your own DNS provider" category. 
+
+
+# Setup
+
+## Setup the custom capability
+
+** IMPORTANT: this is only meant to be used in a space with 1 replica and with a cluster group that only has 1 cluster, see more details in the FAQ**
+
+Due to limitations in TPK8s today in order to add a custom package repo that contains custom capabilties a workaround is needed. These instructions outline that workaround. This workaround is only done once by the platform engineer, once the repo is added everything can be done through TPK8S normally. This will need to be done per cluster group.This workaround will allow for the pkgr to automatcally be installed on the clustergroup, this works around the lack of package repo syncing today and prevents the user from having to manually create pkgrs on indiviual clusters. The workaround does the following:
+
+* adds the package repo and secret to TPK8s project
+* adds the package repo and secret to the tpk8s cluster group
+
+### Configure the tpk8s project with the custom repo
+
+This sets up the the project to have access to the custom pkgr so that it will show up in the UI when looking for capabilties
+
+1. add the package repo and secret to the project
+```bash
+tanzu project use <your-proj>
+export KUBECONFIG=~/.config/tanzu/kube/config
+kubectl apply -f tpk8s-resources/tpk8s-dns-repo.yml
+
+```
+
+### Configure the cluster group with the pkgr
+
+This is needed becuase we need the cluster group to have access to to the pkgr. becuase the pkgr is private we need to sync a secret out to the unerlying clusters.
+
+** make sure you cluster group only contains 1 cluster**
+
+1. add pkgr and secret to the cluster group
+
+```bash
+tanzu ops clustergroup use <your-cg>
+export KUBECONFIG=~/.config/tanzu/kube/config
+kubectl apply -f tpk8s-resources/tpk8s-dns-repo.yml
+
+```
+
+## Deploy the capability
+
+### template the values file with the secret details
+
+in this example we will use azure. full steps found [here](https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/azure.md#service-principal)
+
+copy the values-example.yml to capability-values.yml and update the contents. then run the below command
+
+```bash
+tanzu ops clustergroup use <your-cg>
+export KUBECONFIG=~/.config/tanzu/kube/config
+ ytt -f templated-resources/external-dns-values.yml --data-values-file capability-values.yml  | kubectl apply -f- 
+```
+
+### Deploying the Capability
+
+This can be done through the UI or the api. The steps below use the cli/api so that they can eb easily reproduced. This assumes you already have an availability target.
+
+1. install the capability on the cluster group
+
+```bash
+tanzu ops clustergroup use <your-cg>
+export KUBECONFIG=~/.config/tanzu/kube/config
+k apply -f tpk8s-resources/external-dns-values.yml
+k apply -f tpk8s-resources/dns-capability.yml
+```
+
+
+## Deploy the controller
+
+### Create the space and profile
+
+1. create a profile for the tpk8s-dns controller
+
+```bash
+tanzu project use <your-project>
+export KUBECONFIG=~/.config/tanzu/kube/config
+k apply -f tpk8s-resources/profile.yml
+```
+
+2. create a space using the profile. you will need to update the availability target in the yaml below as well as any profiles you need
+```bash
+tanzu project use <your-project>
+export KUBECONFIG=~/.config/tanzu/kube/config
+k apply -f tpk8s-resources/space.yml
+```
+
+
+
+## Deploying in Tanzu Platform for K8s
+
+This section outline how to deploy this as a part of a space in the platform. This is the recommended approach for running this.
+
+###  Deploy the controller to a space
+
+
+1. connect to your project and the space that was previsouly created
+```bash
+tanzu project use <project>
+tanzu space use tpk8s-dns-controller-space
+```
+2. copy the `templated-resources/secret-example.yml` into the `.tanzu/config` directory and rename it `secret.yml`
+3. Update all of the values in the `secret.yml` 
+4. `tanzu deploy --from-build build-output`
+
+### Validating it works
+
+you can check the logs on the controller pod in the cluster to make sure it is working along with the external-dns logs.
+
+# FAQ
+
+## Why can I only use this with 1 replica and 1 cluster?
+
+This is due to a limitation with external dns and the way TPk8s expects to work. external DNS uses a txt-owner-id to detrmine which records it owns. wehn deployed into a cluster group as a capability this owner is the same between clusters. this means every cluster ijn the cluster group is trying to update the same domain. when a cluster is running external DNS but does not have a space replica on it, it will think there are no DNS entries and remove them. Thsi will create a constant conflict between clusters tryign to delete and update records. You could have the same number of space replicas as clusters and it would not create this issue, however that could lead to other issues if a space re-schedule, etc. for this reason the safest way to deploy this is to ensure the cluster group only has 1 cluster and the space only has 1 replica.
+
+## What if I need to replace the cluster it's running on?
+
+This takes a careful appropach in order to not cause DNS downtime. First edit your values file for the external DNS capability and set the policy to `upsert-only` this will prevent records from being deleted. Next create a new cluster in your cluster group, becuase it's in upsert only mode the new cluster's external dns controller will not try to delete records. next scale your space to 2 replicas. at this point both clusters should be sycning the records and there should be no errors. you can now cordon and drain the old cluster. lastly update the policy back to `sync`.
+
+## how does this work with multiple space replicas?
+
+This is an underlying limitation of external-dns. It does not support running multiple replicas therefore it is not recommended to run this with multiple replicas.
+
+## How does this handle space re-scheduling?
+
+It is recommended to run this with `rollingUpdate` as the strategy for spaces. If this is deployed with external dns using `policy: sync` there is a possibility of having a record deleted and the recreate cuasing minimal downtime. If this is run with `policy: upsert-only` it will not delete records so there will not be an issue of potetnial record recreation, however records will not be deleted and deletes will need to be handled manually. 
+
+
+
+# Known issues
+
+# Cross cloud k8s provider DNS issues
+
+Certain cloud providers like EKS use hostnames for their address when creating a service type LB. This is an issue becuase it is not possible to create a single record that has hostnames and IP addresses. in some DNS providers this can we worked around by using CNAMEs , however some providers do not allow multiple entries with the same DNS fqdn(Azure for exmaple). due to this limitation and lack of consistency between providers this controller can only work aross providers that use the same underlying format for thier addresses. mostly this is an issue with combining EKS and any other provider in the same space. If you are using EKS it's better to use the native route53 integration.
---
+++ dns-controller/src/gslb.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import logging
+import requests
+import json
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+from kubernetes import client, config,utils,dynamic
+from kubernetes.client.rest import ApiException
+import jwt
+import yaml
+import ipaddress
+
+import requests
+import urllib3
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+gslb_sched = BlockingScheduler()
+# Disable certificate warnings
+
+access_token = None
+access_token_expiration = None
+    
+
+
+if hasattr(requests.packages.urllib3, 'disable_warnings'):
+    requests.packages.urllib3.disable_warnings()
+
+if hasattr(urllib3, 'disable_warnings'):
+    urllib3.disable_warnings()
+
+def is_valid_ip(address):
+    try:
+        ipaddress.ip_address(address)
+        return True
+    except ValueError:
+        return False
+
+def delDnsEndpoints(spaceClient: client.ApiClient,record):
+    api = client.CustomObjectsApi(spaceClient)
+    try:
+        api.delete_namespaced_custom_object(group="externaldns.k8s.io", version="v1alpha1",plural="dnsendpoints", namespace="default",name=record)
+        logging.info(f"deleted entry for {record}")
+    except ApiException as e:
+        logging.error(f"failed to delete entry for {record}")
+        raise
+
+def getDnsEndpoints(spaceClient: client.ApiClient):
+    api = client.CustomObjectsApi(spaceClient)
+    try:
+        dnsendpoints = api.list_namespaced_custom_object(group="externaldns.k8s.io", version="v1alpha1",plural="dnsendpoints", namespace="default")
+        return dnsendpoints
+    except ApiException as e:
+        logging.error(f"failed to get list of dns endpoints")
+        raise
+
+
+def apply_dict(spaceClient: client.ApiClient , manifest: dict):
+    api_version = manifest.get("apiVersion").split("/")
+    group = api_version[0]
+    version = api_version[1]
+    kind = manifest.get("kind")
+    resource_name = manifest.get("metadata").get("name")
+    namespace = manifest.get("metadata").get("namespace")
+    api = client.CustomObjectsApi(spaceClient)
+
+    try:
+        api.patch_namespaced_custom_object(group=group, version=version,plural="dnsendpoints", namespace=namespace,name=resource_name,body=manifest)
+        logging.info(f"{namespace}/{resource_name} patched")
+    except ApiException as e:
+        logging.error(f"unable to patch entry {resource_name} {e.body}")
+        if e.reason == "Not Found":
+            try:
+                api.create_namespaced_custom_object(group=group, version=version,plural="dnsendpoints", namespace=namespace,body=manifest)
+                logging.info(f"{namespace}/{resource_name} created")
+            except ApiException as e2:
+                logging.error(f"unable to create entry {resource_name} {e2.body}")
+
+
+def get_domain_bindings(space,ucpClient):
+    managed = managed_domains
+    api = client.CustomObjectsApi(ucpClient)
+    api.api_client.configuration.host = f"{ucpClient.configuration.host}/space/{space}"
+    try:
+        allocated= []
+        bindings = api.list_namespaced_custom_object(group="networking.tanzu.vmware.com", version="v1alpha1",plural="domainbindings",namespace="default")
+        for binding in bindings["items"]:
+            if "status" in binding:
+                for condition in binding["status"]["conditions"]:
+                    if condition["type"] == "DomainAllocated" and condition["status"]:
+                        for md in managed:
+                            if md in binding["spec"]["domain"]:
+                                allocated.append(binding)  
+        return allocated
+
+    except ApiException as e:
+        logging.error(f"failed to get domainbinding data for {space}")
+        raise
+
+
+def getAccessToken(csp_host,csp_token):
+    try:
+        response = requests.post('https://%s/csp/gateway/am/api/auth/api-tokens/authorize?refresh_token=%s' % (csp_host,csp_token))
+        response.raise_for_status()
+    except Exception as e:
+        logging.error(e)
+        return None
+    else:
+        access_token = response.json()['access_token']
+        expires_in = response.json()['expires_in']
+        expire_time = time.time() + expires_in
+        return access_token, expire_time
+
+
+def set_global_token():
+    logger.info("checking if token is expired")
+    global access_token_expiration
+    global access_token
+    if time.time() > access_token_expiration:
+        logger.info("udpating refresh token")
+        access_token, access_token_expiration =  getAccessToken(csp_host,csp_token)
+
+def get_global_token():
+    return access_token
+    
+
+def run():
+    
+    gslb_data = {}
+
+    set_global_token()
+   
+
+    logger.info("creating ucp client")
+    ucpConfig = client.Configuration()
+    ucpConfig.verify_ssl = True
+    ucpConfig.host = f"{tp_host}/org/{org_id}/project/{project_id}"
+    ucpConfig.api_key = {"authorization": "Bearer " + access_token}
+    ucpClient = client.ApiClient(ucpConfig)
+    project_bindings = []
+    for space in spaces:
+        ucpClient.configuration.host = f"{tp_host}/org/{org_id}/project/{project_id}"
+        logger.info(f"generating space gslb data for {space}")
+        
+        domainBindings = get_domain_bindings(space,ucpClient)
+        project_bindings +=domainBindings
+        print(domainBindings)
+       
+
+    desiredServices = []
+
+    #reconcile domain bindings into unique domains and their pools members for now everything is round robin
+    for binding in project_bindings:
+        domain = binding["spec"]["domain"]
+        members = []
+        for address in binding["status"]["addresses"]:
+            member = address["value"]
+            if address["type"] == "IPAddress":
+                recordType = "A"
+            else:
+                recordType = "CNAME"
+            if domain in gslb_data:
+                if gslb_data[domain]["record"]["spec"]["endpoints"][0]["recordType"] != recordType:
+                    logging.error(f"unable to update record for {domain}, coflicting entry types. CNAME and A record detected")
+                    continue
+                if member not in gslb_data[domain]["record"]["spec"]["endpoints"][0]["targets"]:
+                    gslb_data[domain]["record"]["spec"]["endpoints"][0]["targets"].append(member)
+            else:
+                members.append(member)
+            
+                record =  {
+                    "apiVersion": "externaldns.k8s.io/v1alpha1",
+                    "kind": "DNSEndpoint",
+                    "metadata": {
+                        "name": domain,
+                        "namespace": "default"
+                    },
+                    "spec": {
+                        "endpoints": [
+                        {
+                            "dnsName": domain,
+                            "recordTTL": 30,
+                            "recordType": recordType,
+                            "targets": members
+                        }
+                        ]
+                    }
+                }   
+
+                gslb_data[domain] = {}
+                gslb_data[domain]["record"] = record
+
+    for _, serv in gslb_data.items():
+
+        desiredServices.append(serv['record']['metadata']['name'])
+        try:
+            ucpClient.configuration.host = f"{tp_host}/org/{org_id}/project/{project_id}/space/{deploy_space}"
+            logging.debug(serv)
+            logging.info(ucpClient.configuration.host)
+            apply_dict(ucpClient,serv["record"])
+        except Exception as ex:
+            raise Exception('%s' % (ex))
+    
+
+    # delete any records that are no longer needed 
+    try:
+        #get current records
+        ucpClient.configuration.host = f"{tp_host}/org/{org_id}/project/{project_id}/space/{deploy_space}"
+        current_records = getDnsEndpoints(ucpClient)
+        print(current_records)
+        for entry in current_records["items"]:
+            if entry["metadata"]["name"] not in desiredServices:
+                logger.info(f"deleteing {entry["metadata"]["name"]}")
+                delDnsEndpoints(ucpClient,entry["metadata"]["name"])                
+    except Exception as ex:
+            raise Exception('%s' % (ex))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--tphost',
+                        help='FQDN or IP address of the Tanzu Platform API,including the scheme',default=os.environ.get('TP_HOST'))
+    parser.add_argument('--spaces',
+                        help='comma separated list of spaces to watch',default=os.environ.get('SPACES'))
+    parser.add_argument('--projectid',
+                        help='id of the project to use',default=os.environ.get('PROJECT_ID'))
+    parser.add_argument('--manageddomains',help='comma separated list of domains that should be managed',default=os.environ.get('MANAGED_DOMAINS'))
+    parser.add_argument('--project',
+                        help='name of the project',default=os.environ.get('PROJECT'))
+    parser.add_argument('--csptoken', help='CSP token for api calls',default=os.environ.get('CSP_TOKEN'))
+    parser.add_argument('--deploymentspace', help='space name that the controller is deployed to',default=os.environ.get('DEPLOYMENT_SPACE'))
+
+
+
+    args = parser.parse_args()
+
+    if args:
+        # If not specified on the command-line, prompt the user for the
+        # controller IP address and/or password
+
+        csp_token = None
+        csp_host = None
+        tp_host = args.tphost
+        spaces_list = args.spaces
+        project_id = args.projectid
+        project_name = args.project
+        managed_domains = args.manageddomains.split(",")
+        spaces = spaces_list.split(",")
+        csp_token = args.csptoken
+        csp_host = "console.tanzu.broadcom.com"
+        deploy_space = args.deploymentspace
+        
+        try:
+            logging.info("getting initial token")
+            access_token, access_token_expiration = getAccessToken(csp_host,csp_token)
+            if access_token is None:
+                raise Exception("Request for access token failed.")
+        except Exception as e:
+            logging.error(e)
+        else:
+            logging.info("access token recieved")
+
+        decoded = jwt.decode(access_token, options={"verify_signature": False})
+        org_id = decoded['context_name']
+        
+        gslb_sched.add_job(id='run gslb job',func=run,trigger='interval',seconds=10)
+        gslb_sched.start()
+
+    else:
+        parser.print_help()
+
+    
